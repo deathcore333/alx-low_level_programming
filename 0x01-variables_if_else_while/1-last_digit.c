05866c25a00741a3898b52a069c1443baa8f5476
Amos Maina <amossmwangii@gmail.com>
2022-09-09 10:57:39 -0700

    first if else code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,32 @@
-include <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Prints out the value of a randomly generated number and assesses
+ * whether it is greater than 5, less than 6 or equal to zero.
+ *
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	
+	if ((n % 10) > 5)
+	 {
+		 printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	  }
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	}
+	else 
+	{
+		printf("Last digit of %d is 0 and is 0\n", n);
+	}
 	return (0);
 }
