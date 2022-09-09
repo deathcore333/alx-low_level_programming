c8431109cde502239ae124f6b3bba73586d6ff6d
Amos Maina <amossmwangii@gmail.com>
2022-09-09 11:58:00 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a function that prints out the leters of the alphabet
+ * UPPERCASE and lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	
+		putchar(i);
+	
+	for (i = 'A'; i <= 'Z'; i++)
+	
+		putchar(i);
+	
+	putchar('\n');
+	return (0);
+}
