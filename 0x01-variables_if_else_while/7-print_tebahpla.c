e3b0fa44dafe8c90ffcba510f12aaa26e3b18de0
Amos Maina <amossmwangii@gmail.com>
2022-09-11 03:19:12 -0700

    worked faster

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the letters in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
