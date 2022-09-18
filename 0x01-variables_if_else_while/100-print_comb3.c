a55d8fd1e30df61ffe0af74c08eb25a99e8733d6
Amos Maina <amossmwangii@gmail.com>
2022-09-17 23:31:26 -0700

    double digits print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a function that prints all the possible combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 10; digit1++)
+	{
+
+		for (digit2 = 0; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if(digit1 == 9 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
