60f6d42b04624c114584ea103c24cc8fdb0291ad
Amos Maina <amossmwangii@gmail.com>
2022-09-21 04:07:24 -0700

    pretty sure its wrong

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - program that prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int _putchar(char c)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
+}
