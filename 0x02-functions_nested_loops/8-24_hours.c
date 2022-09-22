3b2d2255d6526ae6b0a25da0d73156d892f052a5
Amos Maina <amossmwangii@gmail.com>
2022-09-22 04:47:02 -0700

    must be right

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,14 +7,14 @@
 void jack_bauer(void)
 {
 	int a, b, c, d;
-	
+
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 3; b++)
 		{
-			if (a <= 2 && b <=3)
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
-				for(c = 0; c <= 5; c++)
+				for (c = 0; c <= 5; c++)
 				{
 					for (d = 0; d <= 9; d++)
 					{
@@ -23,6 +23,7 @@ void jack_bauer(void)
 						_putchar(58);
 						_putchar(c + '0');
 						_putchar(d + '0');
+						_putchar('\n');
 					}
 				}
 			}
