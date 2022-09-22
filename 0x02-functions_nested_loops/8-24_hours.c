b3ced1dd67f5301e6743a37f7c75a3e66876c608
Amos Maina <amossmwangii@gmail.com>
2022-09-22 05:03:21 -0700

    test trial

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,21 +12,20 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 3; b++)
 		{
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+			for (c = 0; c <= 5; c++)
 			{
-				for (c = 0; c <= 5; c++)
+				for (d = 0; d <= 9; d++)
 				{
-					for (d = 0; d <= 9; d++)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(58);
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(58);
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
+		
 				}
 			}
 		}
+
 	}
 }
