6f81ba24684343376272e936ef32f8f7a1a468d7
Amos Maina <amossmwangii@gmail.com>
2022-09-22 01:45:03 -0700

    meeehn

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks if the c is lowercase
+ * @c is the letter under test
+ *
+ * Return: Always 0 
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
