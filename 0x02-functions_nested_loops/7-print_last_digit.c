a075c49744322dc9109c1bbc6fbdd96facc50cbc
Amos Maina <amossmwangii@gmail.com>
2022-09-22 04:09:13 -0700

    something something

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,16 +3,16 @@
 /**
  * print_last_digit - a program that prints the last digit of a number
  *@n: the number under testing
- * Return: Always 0
+ * Return: Always last digit
  */
 
 int print_last_digit(int n)
 {
 	n = n % 10; 
 	if (n < 0)
-	n = -n;
 	{
-		_putchar(n + '0');
-		return (0);
+		n = -n;
 	}
+	_putchar(n + '0');
+	return (n);
 }
