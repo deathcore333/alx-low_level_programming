000c2cd6cc852024ac4fa18ae6b0d96df394b2ee
Amos Maina <amossmwangii@gmail.com>
2022-09-22 06:34:58 -0700

    remmember how to type two digit numbers

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - prints the  times table
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int product;
+
+	for (i = 1; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			product = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else if (product < 10)
+			{
+				_putchar(' ');
+				_putchar((product % 10) + '0');
+			}
+		
+		}
+		_putchar('\n');
+	}
+}
