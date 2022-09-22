0e8e724d3ee8ec6ea33f4c6eee8d7e3771ee1e07
Amos Maina <amossmwangii@gmail.com>
2022-09-22 11:55:07 -0700

    meehn

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints all the natural numbers below 1024
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+			sum +- i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
