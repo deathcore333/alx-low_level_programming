2a0c3e46760e9ba85933acd68dc6ed58956eafc8
Amos Maina <amossmwangii@gmail.com>
2022-09-22 00:20:22 -0700

    I am back for good

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,10 +6,9 @@
  * Return: Always 0 (Success)
  */
 
+
 int main(void)
 {
-	int _putchar(char c)
-	{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -20,5 +19,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
 }
+
