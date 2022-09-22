fd1292b8831d62646fbfce1b090ebec7429abb40
Amos Maina <amossmwangii@gmail.com>
2022-09-22 13:16:38 -0700

    damn

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -19,17 +19,19 @@ int main(void)
 	{
 
 		fibsum = fib1 + fib2;
-
+		{
 		if (fibsum > 4000000)
-		       break;
-		
+			break;
+		}
+		{
 		if ((fibsum % 2) == 0)
 			sum = fibsum + sum;
-		
+		}
+
 		fib1 = fib2;
 		fib2 = fibsum;
 	}
 	printf("%.0f\n", sum);
 
 	return (0);
-}	
+}
