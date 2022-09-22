6b6d4c39e1595a6db06f22a1fc6fc365fbe20fac
Amos Maina <amossmwangii@gmail.com>
2022-09-22 01:21:16 -0700

    bababababa

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,17 @@
  */
 
 void print_alphabet_x10(void)
-{	
+{
 	int i;
 	int c;
 
-	for (i = 0;i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
