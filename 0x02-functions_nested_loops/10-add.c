8c460cbb8cc2fa02ac12f2daab5316e10fb2bca9
Amos Maina <amossmwangii@gmail.com>
2022-09-22 06:59:33 -0700

    do we need all this

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,6 +4,7 @@
  * add - a function that conducts additions
  *@a: first number
  *@b: second number
+ *Return: add
  */
 
 int add(int a, int b)
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -33,7 +33,7 @@ void times_table(void)
 			{
 				_putchar(' ');
 				_putchar((product % 10) + '0');
-			}	
+			}
 		}
 		_putchar('\n');
 	}
