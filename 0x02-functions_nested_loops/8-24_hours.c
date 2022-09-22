d27a469623164e9932c78a992643b897886c252f
Amos Maina <amossmwangii@gmail.com>
2022-09-22 05:14:54 -0700

    difficult af

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,26 +6,19 @@
 
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int hrs;
+	int mins;
 
-	for (a = 0; a <= 2; a++)
+	for (hrs = 0; hrs < 24; hrs++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (mins = 0; mins < 60; mins++)
 		{
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(58);
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-		
-				}
-			}
-		}
-
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}		
 	}
 }
