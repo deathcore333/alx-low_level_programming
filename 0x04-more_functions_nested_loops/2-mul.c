cb2b87439550090affb937c58530ac8ac1353840
Amos Maina <amossmwangii@gmail.com>
2022-09-25 08:51:58 -0700

    hellow

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - a program that multiplies two intages and gives the solution
+ *@a: first integer
+ *@b: second integer
+ *
+ */
+
+int mul(int a, int b)
+{
+	int product;
+	
+	product = a * b;
+
+	return (product);
+}
