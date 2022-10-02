c9e1c9e597ea08995caf8d014d0cae22f74bcae4
Amos Maina <amossmwangii@gmail.com>
2022-10-02 06:13:37 -0700

    new commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,14 +14,13 @@ void print_square(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
-			{
+
 				_putchar('#');
 
 			if (i == size - 1)
 				continue;
 			_putchar('\n');
 
-			}
 		}
 	}
 
