a0078c4e540bc227c56f5747eb336d23e9b3973c
Amos Maina <amossmwangii@gmail.com>
2022-10-03 05:04:14 -0700

    made it through

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - a function that tests and prints whether an integer is positive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+lessons on debbugint
