1a4e2c235b9a32d3d839ca0d4826779c346779a1
deathcore333 <100504477+deathcore333@users.noreply.github.com>
2022-10-03 15:10:50 +0300

    new changes

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,1 +1,24 @@
-#
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+/* no incremment set for int i hence the program is stuck in a loop that prints 0 */
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}
