ba20283409966e02277d0f46eac2480806f890ef
Amos Maina <amossmwangii@gmail.com>
2022-10-08 11:54:15 -0700

    new commitment

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that converts all lowercase letters of a string to uppercase
+ *
+ * Return: string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+
+	return (s);
+}
