a8affa738b322abb9f480b8eab6a2e4f2c9cd3ce
Amos Maina <amossmwangii@gmail.com>
2022-10-08 15:48:55 -0700

    new change

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer
+ * @n: the integer to print
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
