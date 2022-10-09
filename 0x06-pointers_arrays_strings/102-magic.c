832f47199ca3d9bcfdca95f147a228ea3fd7c746
deathcore333 <100504477+deathcore333@users.noreply.github.com>
2022-10-09 15:34:14 +0300

    new files

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+
+  *(p + (sizeof(n) + 1)) = 98;
+
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
