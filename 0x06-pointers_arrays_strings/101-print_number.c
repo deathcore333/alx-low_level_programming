1e31070bbe4f03c749506f67ba58900ca8f58bda
Amos Maina <amossmwangii@gmail.com>
2022-10-09 05:19:06 -0700

    sure

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,17 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	int a;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		a = n * -1;
 	}
+	else
+		a = n;
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
-	_putchar((num % 10) + '0');
+	if ((a / 10) > 0)
+		print_number(a / 10);
+	_putchar((a % 10) + '0');
 }
