129fc6392f7200f1eb964ba9890d818994dbed6f
Amos Maina <amossmwangii@gmail.com>
2022-10-10 02:36:28 -0700

    i hope it works

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strstr - a function that locates a substring
+ * @haystack: the string
+ * @needle: the substring
+ * 
+ * Return: pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
