5930e83188c7336ff81f59d73a477c109b96aea6
Amos Maina <amossmwangii@gmail.com>
2022-10-10 15:32:23 -0700

    hotfix

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the string length
+ * @s: the string
+ *
+ * Return: string length 
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s)
+	{
+		count++;
+		count += _strlen_recursion(s + 1);
+	}
+
+	return (count);
+}
