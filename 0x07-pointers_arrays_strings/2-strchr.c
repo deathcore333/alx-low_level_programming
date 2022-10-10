896214748fdb70245c46b7812dc207103b95738a
Amos Maina <amossmwangii@gmail.com>
2022-10-10 01:20:59 -0700

    new values

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: string 
+ * @c: character
+ *
+ * Return: c or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + 1); i++)
+	{
+		if (*(s + 1) == c)
+			return (s + i);
+	}
+	return (0);
+}
