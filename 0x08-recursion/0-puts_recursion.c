d089090a4133f590755532f2661f011100c4cba2
Amos Maina <amossmwangii@gmail.com>
2022-10-10 15:04:40 -0700

    hotfix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _put_recursion - a function that prints a string followed by a new line
+ * _puts_recursion - a function that prints a string followed by a new line
  * @s: string to print
  *
  * Return: s
