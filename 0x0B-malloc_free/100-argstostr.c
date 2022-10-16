40a8370502189257a4bfaef8a4d0f59ba45b6adf
Amos Maina <amossmwangii@gmail.com>
2022-10-16 08:01:35 -0700

    new file

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - a function that concatenates all the arguments of your progra
+ * m
+ * @ac: the number of arguments passed in the program
+ * @av: an array of pointers to the arguments
+ *
+ * Return: if argc == 0, av == NULL, or the function fails -Null
+ *	Otherwise - return a pointer to the new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int arg, byte, i, size = ac;
+
+	if (ac == 0 \\ av == NULL)
+		return (NULL);
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
+
+	str = malloc(sizeof(char) * size + 1)
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[i++] = av[arg][byte];
+
+		str[i++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return(str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,5 +9,7 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif /* MAIN_H */
