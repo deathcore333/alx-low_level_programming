6268c408576f18f5ec543814d1942ec489507d77
Amos Maina <amossmwangii@gmail.com>
2022-10-18 13:47:16 -0700

    its not easy

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,7 @@
  * malloc_checked - a function that allocates memory using malloc
  * @b: number of bytes to allocate
  *
- * Return: pointer to the allocated memory 
+ * Return: pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - a function that concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
+ * @n: integer
+ *
+ * Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, len = 0;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i]; i++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+
+	if (concat == NULL)
+		return(NULL);
+
+	len = 0;
+
+	for (i = 0; s1[i]; i++)
+		concat[len++] = s1[i];
+
+	for (i = 0; s2[i]; i++)
+		concat[len++] = s2[i];
+
+	concat[len] = '\0';
+
+	return (concat);
+	
+}
