628c79df5f07da9ba81a9417b8eb4bed36b15732
deathcore333 <100504477+deathcore333@users.noreply.github.com>
2022-10-19 00:10:24 +0300

    new lines of code added

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,33 +12,45 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, len = 0;
-	char *concat;
+	unsigned int i, j, a = 0;
+	char *c;
 
-	if (s1 == NULL)
+	if (s1 == 0)
 		s1 = "";
-	if (s2 == NULL)
+	if (s2 == 0)
 		s2 = "";
 
 	for (i = 0; s1[i]; i++)
-		len++;
+		;
+	for (j = 0; s2[j]; j++)
+		;
 
-	concat = malloc(sizeof(char) * (len + 1));
+	if (j > n)
+		j = n;
 
+	c = malloc ((sizeof(char) * i) + (sizeof(char) * j) +1);
 
-	if (concat == NULL)
-		return(NULL);
+	if (c == NULL)
+	{
+		return (NULL);
+	}
 
-	len = 0;
+	while (*s1 != '\0')
+	{
+		c[a] = *s1;
+		s1++;
+		a++;
+	}
 
-	for (i = 0; s1[i]; i++)
-		concat[len++] = s1[i];
+	while (a < j + i)
+	{
+		c[a] = *s2;
+		s2++;
+		a++;
+	}
 
-	for (i = 0; s2[i]; i++)
-		concat[len++] = s2[i];
+c[i + j] = '\0';
 
-	concat[len] = '\0';
-
-	return (concat);
+return (c);
 	
 }
