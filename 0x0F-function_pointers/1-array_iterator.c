8750e6c29b2aa5ee3a43dc10008e5d7f379c3883
Amos Maina <amossmwangii@gmail.com>
2023-01-08 23:35:29 -0800

    always check the syntax

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@
  * @action - pointer to the needed function
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 	if (!array || !action)
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@
  * Return: -1 if no lements match or 0 otherwise
  */
 
-int int_index(int *array, int size, int (*cmp)(int));
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
 
