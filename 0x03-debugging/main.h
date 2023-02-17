f3e57171559d66a9a4f5d78cfecd2ac81c25b46d
Amos Maina <amossmwangii@gmail.com>
2023-02-17 04:20:35 -0800

    new file changes on debbuggin

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,4 +5,8 @@
 
 void positive_or_negative(int i);
 
-#endif /* MAIN_H */
+int largest_number(int, int, int);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif
