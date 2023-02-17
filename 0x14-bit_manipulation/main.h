4a8010fa1a02a9e315f5769e188b4e2d266e39ea
Amos Maina <amossmwangii@gmail.com>
2023-02-17 06:49:59 -0800

    extensive changes in the main file description

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,6 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
+#ifndef MAIN_H
+#define MAIN_H
 /*
- * File: holberton.h
- * AUth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x13-bit_manipulation directory.
  */
@@ -17,4 +14,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
