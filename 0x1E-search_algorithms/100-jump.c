c832467e513d325336cae76c43e1a0c5c9e623d5
deathcore333 <amossmwangii@gmail.com>
2023-04-12 15:02:42 +0300

    function that implements the jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searches array using jump_search algo
+ * @array: pointer to first element
+ * @size: number of elements in array
+ * @value: search element
+ *
+ * Return: -1 if array is NULL or element is absent
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t jump, step;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	step = sqrt(size);
+	for (i = jump = 0; jump < size && array[jump] < value;)
+	{
+		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += step;
+	}
+
+	printf("Value found between indexes [%ld] = [%d]\n", i, jump);
+	jump = jump < size - 1 ? jump : size - 1;
+	for (; i < jump && array[i] < value; i++)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+	return (array[i] == value ? (int)i : -1);
+}
