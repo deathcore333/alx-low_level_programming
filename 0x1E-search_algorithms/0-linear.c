1918bf4719436ce3e1f0972b08b7862513ba6d36
deathcore333 <amossmwangii@gmail.com>
2023-04-12 13:31:27 +0300

    corrected iteration error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, *(array + i));
-		if (*(array + 1) == value)
+		if (*(array + i) == value)
 		return (i);
 	}
 
